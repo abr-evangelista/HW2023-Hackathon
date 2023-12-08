48d0dcf11e0163303828c6b0d2987eef0b6a8179
abr-evangelista <gabrielgoncalves@estudante.ufscar.br>
2023-12-08 19:56:02 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import io
+
+print("Bem vindo ao Cofre de Senhas Super Simplificado e Seguras (confiável!!!!!)")
+user_login = input("Digite seu nome de usuário: ")
+user_password = input("Digite sua senha: ")
