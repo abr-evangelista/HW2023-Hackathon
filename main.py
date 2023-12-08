2b66fa59ff07161ecc106a6d01a8cb71901bd5e9
EriikGabriel <erikgabriel.lins@hotmail.com>
2023-12-08 20:44:51 -0300

    Verifica existência dos arquivos

--- main.py
+++ main.py
@@ -1,15 +1,63 @@
+# TODOS #
+# Puxar o senhas.txt
+# verificas se files n existem pra criar novos users
+# transforme as strings da senhas em 
+
 import io
+import os
+import hashlib
+
+def compute_sha256_hash(data):
+    # Create a SHA-256 hash object
+    sha256_hash = hashlib.sha256()
+
+    # Update the hash object with the bytes-like object
+    sha256_hash.update(data.encode('utf-8'))
+
+    # Get the hexadecimal representation of the hash
+    hash_result = sha256_hash.hexdigest()
+
+    return hash_result
 
 print("Bem vindo ao Cofre de Senhas Super Simplificado e Seguras (confiável!!!!!)")
-user_login = input("Digite seu nome de usuário: ")
-user_password = input("Digite sua senha: ")
+user_login = input("Digite seu nome de usuário (sem letras maiusculas): ")
+user_password = input("Digite sua senha (min 8 caracteres): ")
 
-file_name = "login.txt"
+PUBLIC_KEY_FILE_NAME = "public_key.txt" # valor que vai ajudar pra descriptografar as senhas
+PASSWORDS_FILE_NAME = "senhas.txt" # senhas do cofre
+
+public_key_exists = False
+passwords_exists = False
+
+# Verifica se o arquivo existe e cria um novo se não existir
+if not os.path.exists(PUBLIC_KEY_FILE_NAME):
+  with open(PUBLIC_KEY_FILE_NAME, "w") as f:
+    f.write("")
+
+if not os.path.exists(PASSWORDS_FILE_NAME):
+  with open(PASSWORDS_FILE_NAME, "w") as f:
+    f.write("")
+
+public_keys = []
+passwords = []
 
 try:
-    with open(file_name, 'r') as file:
-        file_content = file.read()
+  with open(PUBLIC_KEY_FILE_NAME, 'r') as file:
+      for linha in file:
+        public_keys.append(linha)
 except FileNotFoundError:
-    print(f"File '{file_name}' not found.")
+  print(f"Arquivo '{PUBLIC_KEY_FILE_NAME}' não encontrado.")
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f"Ocorreu um erro: {e}")
+
+try:
+  with open(PASSWORDS_FILE_NAME, 'r') as file:
+    for linha in file:
+      passwords.append(linha)
+except FileNotFoundError:
+    print(f"Arquivo '{PASSWORDS_FILE_NAME}' não encontrado.")
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
+
+print(public_keys)
+print(passwords)
