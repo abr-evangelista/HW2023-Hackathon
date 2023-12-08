f06c624280a59371bd47401e6c92c5c421860d7d
abr-evangelista <gabrielgoncalves@estudante.ufscar.br>
2023-12-08 20:01:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import io
 
-print("Bem vindo ao Cofre de Senhas Super Simplificado e Seguras (confiável!!!!)")
+print("Bem vindo ao Cofre de Senhas Super Simplificado e Seguras (confiável!!!!!)")
 user_login = input("Digite seu nome de usuário: ")
 user_password = input("Digite sua senha: ")
+
+file_name = "login.txt"
+
+try:
+    with open(file_name, 'r') as file:
+        file_content = file.read()
+except FileNotFoundError:
+    print(f"File '{file_name}' not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
